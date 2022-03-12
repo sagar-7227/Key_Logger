15e9abdc6e430b5c2b4c9f007e9db46362fab04e
sagar-7227 <sagarvashnav72427@gmail.com>
2022-03-12 17:08:26 +0530

    main

---
+++ cryptography/DecryptFile.py
@@ -0,0 +1,26 @@
+from cryptography.fernet import Fernet
+
+key = "" # get it from the generatekey.py file 
+
+system_information_e = 'e_systeminfo.txt'
+clipboard_information_e = 'e_clipboard.txt'
+keys_information_e = 'e_key_log.txt'
+
+
+
+encrypted_files = [system_information_e, clipboard_information_e, keys_information_e]
+count = 0
+
+
+for decrypting_files in encrypted_files:
+
+    with open(encrypted_files[count], 'rb') as f:
+        data = f.read()
+
+    fernet = Fernet(key)
+    decrypted = fernet.decrypt(data)
+
+    with open("decryption.txt", 'ab') as f:
+        f.write(decrypted)
+
+    count += 1
